6fc69b9bb22b36d374086b440a04ceb47e169c8c
ivz-dev <zakutynsky@yahoo.com>
2018-08-17 17:36:42 +0300

    separate input

--- src/components/headerSearch/index.js
+++ src/components/headerSearch/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { HeaderSearchWrapper } from "./style";
+import Input from "../input";
 
 class HeaderSearch extends Component {
     render() {
         return (
             <HeaderSearchWrapper>
-                <input type="text" placeholder="Search a Device" />
+                <Input type="text" placeholder="Search a Device" />
             </HeaderSearchWrapper>
         )
     }
--- src/components/headerSearch/style.js
+++ src/components/headerSearch/style.js
@@ -2,16 +2,4 @@ import styled from "styled-components";
 
 export const HeaderSearchWrapper = styled.div`
     width: 250px;
-
-    input {
-        width: 100%;
-        border: 1px solid #E4E8EA;
-        padding: 10px;
-        border-radius: 5px;
-        box-sizing: border-box;
-
-        &::placeholder { 
-            color: #c1c5c8;
-        }
-    }
 `;
